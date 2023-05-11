fdf8e58b61f34c7e1585539c6ed6d32771d1f337
Kevin King <kevin.m.king@icloud.com>
2023-05-11 17:19:15 -0400

    added 10qs

---
+++ data/extract-api.py
@@ -0,0 +1,89 @@
+import json
+from sec_api import QueryApi
+from sec_api.index import ExtractorApi 
+
+# To run the program in terminal: python3 test-api.py
+
+# API Keys
+queryApi = QueryApi(api_key="01a45f1a399cfae01bab0ebe6c90a8c8bb320c46f96180635eb208f16b2d65e0")
+extractorApi = ExtractorApi("01a45f1a399cfae01bab0ebe6c90a8c8bb320c46f96180635eb208f16b2d65e0")
+
+
+with open("companies.json", "r") as companies_json_file: 
+  data = json.load(companies_json_file)
+
+companies = data["Companies"]
+forms = data["Forms"]
+
+for i in range(0, len(companies)):
+  for j in range(0, len(forms)):
+    companyName = companies[i]["companyName"]
+    ticker = companies[i]["ticker"]
+    formType = forms[j]["formType"]
+    formDates = forms[j]["dates"]
+
+    quarter = 1
+    for k in range(0, len(formDates)):
+      filedAt = formDates[k]["filedAt"]
+      year = formDates[k]["year"]
+      
+      print(companyName + ": " + year + " " + " " + formType)
+
+      query = {
+        "query": { "query_string": {
+            "query": "ticker:" + ticker + " AND filedAt:" + filedAt + " AND formType:\"" + formType + "\""
+          } },
+        "from": "0",
+        "size": "10",
+        "sort"
+        : [{ "filedAt": { "order": "desc" } }]
+      }
+
+      filings_query = queryApi.get_filings(query)
+
+      # Save SEC filing
+
+      file_path = "filings/" + ticker + "_filings_" + formType + "_" + year + ".json"
+      with open(file_path, "w") as filings_json_file:
+        json.dump(filings_query, filings_json_file)
+
+      print("Filings saved to filings.json.")
+
+      # Read filing JSON 
+      with open(file_path, "r") as filings_json_file:
+        query_data = json.load(filings_json_file)
+
+      # Get the necessary URL for the filing
+      filing_url = ""
+      for l in range(0, len(query_data["filings"])):
+        link = query_data["filings"][l]["formType"]
+        if (link == formType): 
+          filing_url = query_data["filings"][l]["linkToFilingDetails"]
+
+      print("Viewing URL: " + filing_url)
+
+      risk_file_path = ""
+      mda_file_path = ""
+
+      # Extract relevant sections in the filing
+      if (formType == "10-K"): 
+        risk_factors_section = extractorApi.get_section(filing_url, "1A", "text")
+        risk_file_path = "risks/" + ticker + "_risks_" + year
+        mda_section = extractorApi.get_section(filing_url, "7", "text")
+        mda_file_path = "mda/" + ticker + "_mda_" + year
+      
+      elif (formType == "10-Q"): 
+        risk_factors_section = extractorApi.get_section(filing_url, "part2item1a", "text")
+        risk_file_path = "risks/" + ticker + "_risks+" + year + "_q" + str(quarter)
+        mda_section = extractorApi.get_section(filing_url, "part1item2", "text")
+        mda_file_path = "mda/" + ticker + "_mda+" + year + "_q" + str(quarter)
+        quarter += 1
+
+      # Write new text files for the relevant sections
+      with open(risk_file_path, "w", encoding="utf-8") as f:
+        f.write(risk_factors_section)
+
+      with open(mda_file_path, "w", encoding="utf-8") as f:
+        f.write(mda_section)
+
+      print("\n")
--- data/test-api.py
+++ data/test-api.py
@@ -8,30 +8,9 @@ from sec_api.index import ExtractorApi
 queryApi = QueryApi(api_key="d9154fab9f6819c42f9bb716dc24983cd9ca3bb322239596629b7dc05ca4ce78")
 extractorApi = ExtractorApi("d9154fab9f6819c42f9bb716dc24983cd9ca3bb322239596629b7dc05ca4ce78")
 
-tickers = ["TSLA"]
-
-with open("companies.json", "r") as companies_json_file: 
-  data = json.load(companies_json_file)
-
-companies = data["Companies"]
-forms = data["Forms"]
-
-for i in range(0, len(companies)):
-  for j in range(0, len(forms)):
-    companyName = companies[i]["companyName"]
-    ticker = companies[i]["ticker"]
-    formType = forms[j]["formType"]
-    formDates = forms[j]["dates"]
-
-    for k in range(0, len(formDates)):
-      filedAt = formDates[k]["filedAt"]
-      year = formDates[k]["year"]
-      
-      print(companyName + ": " + year + " " + " " + formType)
-
-      query = {
+query = {
         "query": { "query_string": {
-            "query": "ticker:" + ticker + " AND filedAt:" + filedAt + " AND formType:\"" + formType + "\""
+            "query": "ticker: TSLA AND filedAt: {2022-01-01 TO 2022-06-01} AND formType:\"10-Q\""
           } },
         "from": "0",
         "size": "10",
@@ -39,37 +18,33 @@ for i in range(0, len(companies)):
         : [{ "filedAt": { "order": "desc" } }]
       }
 
-      filings_query = queryApi.get_filings(query)
+filings_query = queryApi.get_filings(query)
 
-      # Save SEC filing
-      file_path = "filings/" + ticker + "_filings_" + year + ".json"
-      with open(file_path, "w") as filings_json_file:
-        json.dump(filings_query, filings_json_file)
+file_path = "filings/TSLA_filings_10Q.json"
 
-      print("Filings saved to filings.json.")
+with open(file_path, "w") as filings_json_file:
+    json.dump(filings_query, filings_json_file)
 
-      # Read filing JSON 
-      with open(file_path, "r") as filings_json_file:
-        query_data = json.load(filings_json_file)
-          
-      # Get the necessary URL for the filing
-      filing_url = ""
-      for l in range(0, len(query_data["filings"])):
-        link = query_data["filings"][l]["formType"]
-        if (link == formType): 
-          filing_url = query_data["filings"][l]["linkToFilingDetails"]
+# Read filing JSON 
+with open(file_path, "r") as filings_json_file:
+    query_data = json.load(filings_json_file)
 
-      print("Viewing URL: " + filing_url)
+filing_url = ""
 
-      # Extract relevant sections in the filing
-      risk_factors_section = extractorApi.get_section(filing_url, "1A", "text")
-      mda_section = extractorApi.get_section(filing_url, "7", "text")
+for l in range(0, len(query_data["filings"])):
+    link = query_data["filings"][l]["formType"]
+    if (link == "10-Q"): 
+        filing_url = query_data["filings"][l]["linkToFilingDetails"]
 
-      # Write new text files for the relevant sections
-      with open("risks/" + ticker + "_risks_" + year, "w", encoding="utf-8") as f:
-        f.write(risk_factors_section)
+print("Viewing URL: " + filing_url)
 
-      with open("mda/" + ticker + "_mda_" + year, "w", encoding="utf-8") as f:
-        f.write(mda_section)
-    
-      print("\n")
+# Extract relevant sections in the filing
+risk_factors_section = extractorApi.get_section(filing_url, "part2item1a", "text")
+mda_section = extractorApi.get_section(filing_url, "part1item2", "text")
+
+# Write new text files for the relevant sections
+with open("risks/TSLA_risks_10Q_1", "w", encoding="utf-8") as f:
+    f.write(risk_factors_section)
+
+with open("mda/TSLA_mda_10Q_1", "w", encoding="utf-8") as f:
+    f.write(mda_section)
